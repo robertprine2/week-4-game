781e8db5209e83e5b6f1194dd4f2bb91e6084913
Robert Prine <robertprine2@yahoo.com>
2016-04-28 00:26:53 -0400

    initial with basic html outline and some css

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+//waits for js to start until after html has loaded
+
+$(document).ready(function(){
+
+	// object of characters that can be chosen 
+
+	var game = {
+		
+	}
+
+}); //ends ready function
